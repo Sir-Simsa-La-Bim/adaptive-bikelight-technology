54c867b0821576014e8c2fd119e5415cdb279ff9
Leander <hellerleander@gmail.com>
2026-02-03 13:49:48 +0100

    python scripts fixed

--- src/UserInterfaceDriver/logger.py
+++ src/UserInterfaceDriver/logger.py
@@ -2,8 +2,11 @@ from driver import *
 from cancel_token.token import CancelToken
 import typing
 
-async def runLogger(port: str, path: str, cancelToken: CancelToken|None = None)->None:
-    file = open(path, 'w')
+
+async def runLogger(
+    port: str, path: str, cancelToken: CancelToken | None = None
+) -> None:
+    file = open(path, "w")
 
     synchronizer = None
     try:
@@ -17,15 +20,17 @@ async def runLogger(port: str, path: str, cancelToken: CancelToken|None = None)-
 
         timestampMask = (1 << (IMU_TIMESTAMP.byteSize * 8)) - 1
 
-        print('waiting for connection... ')
+        print("waiting for connection... ")
         await synchronizer.waitForFullSync()
 
-        file.write('time[ms],gyro x[rad/s],gyro y[rad/s],gyro z[rad/s], accel x[m/s^2], accel y[m/s^2],accel z[m/s^2],inv radius[1/m],direction[rad],\n')
-        
+        file.write(
+            "time[ms],gyro x[rad/s],gyro y[rad/s],gyro z[rad/s], accel x[m/s^2], accel y[m/s^2],accel z[m/s^2],inv radius[1/m],direction[rad],\n"
+        )
+
         calibration = typing.cast(ImuCalibration, calibrationParam.getValue())
-        file.write(f',{calibration.gyroXOffset},{calibration.gyroYOffset}\n')
-        
-        print('logging')
+        file.write(f",{calibration.gyroXOffset},{calibration.gyroYOffset}\n")
+
+        print("logging")
 
         lastTimestamp_ms = typing.cast(int, imuTimestampParam.getValue())
         consecutiveTimestamp_ms = 0
@@ -49,18 +54,25 @@ async def runLogger(port: str, path: str, cancelToken: CancelToken|None = None)-
                 directionParam.getValueWait(),
             )
 
-            file.write(f'{consecutiveTimestamp_ms},{imuData.gyroX},{imuData.gyroY},{imuData.gyroZ},{imuData.accelX},{imuData.accelY},{imuData.accelZ},{motionData.invRadius},{direction}\n')
+            file.write(
+                f"{consecutiveTimestamp_ms},{imuData.gyroX},{imuData.gyroY},{imuData.gyroZ},{imuData.accelX},{imuData.accelY},{imuData.accelZ},{motionData.invRadius},{direction}\n"
+            )
 
     finally:
         file.close()
         if synchronizer is not None:
             synchronizer.close()
 
-def runLoggerForever(port: str, path: str)->None:
-    import asyncio
-    asyncio.get_event_loop().run_until_complete(runLogger(port, path))
 
-if __name__ == '__main__':
-    port = input('Run logger on port: ')
-    path = input('Output file: ')
+def runLoggerForever(port: str, path: str) -> None:
+    import asyncio
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(runLogger(port, path))
+
+
+if __name__ == "__main__":
+    port = input("Run logger on port: ")
+    path = input("Output file: ")
     runLoggerForever(port, path)
